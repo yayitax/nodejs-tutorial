515a5825aa53718ab24bbc74926efed654f07c56
Ximena <yayitax_cadena95@hotmail.com>
2019-12-18 13:57:04 -0700

    Finished Getting started and Using npm

---
+++ app/index.js
@@ -0,0 +1,8 @@
+const calc = require('./calc')
+
+const numbersToAdd = [
+    3, 4, 10, 2
+]
+
+const result = calc.sum(numbersToAdd)
+console.log(`The result is ${result}`)
---
+++ index.js
@@ -0,0 +1,6 @@
+require('./app/index')
+
+const _ = require('lodash')
+
+_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
+// → { 'a': 1, 'b': 2, 'c': 3 }
